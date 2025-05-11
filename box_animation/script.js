3375266103e1db76b05a0fa2268191f3143301ad
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-05-11 22:38:23 +0530

    box animation

---
+++ box_animation/script.js
@@ -0,0 +1,14 @@
+gsap.to("#box1", {
+    x:1200,
+    duration:3,
+    delay:1,
+    rotate:360,
+    borderRadius: "50%",
+    backgroundColor: "orange"
+})
+gsap.from("#box2", {
+    x: 1200,
+    duration: 3,
+    delay: 1,
+    backgroundColor: "blue"
+})
