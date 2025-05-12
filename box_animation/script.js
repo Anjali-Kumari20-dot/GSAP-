c6018a6f04a82c9b7272f05a520ae45a61482de1
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-05-12 06:27:57 +0530

    Text Animation

--- box_animation/script.js
+++ box_animation/script.js
@@ -1,14 +1,16 @@
 gsap.to("#box1", {
-    x:1200,
-    duration:3,
-    delay:1,
-    rotate:360,
-    borderRadius: "50%",
-    backgroundColor: "orange"
-})
+  x: 1200,
+  duration: 3,
+  delay: 1,
+  rotate: 360,
+  borderRadius: "50%",
+  backgroundColor: "orange",
+  repeat: -1,
+  yoyo: true,
+});
 gsap.from("#box2", {
-    x: 1200,
-    duration: 3,
-    delay: 1,
-    backgroundColor: "blue"
-})
+  x: 1200,
+  duration: 3,
+  delay: 1,
+  backgroundColor: "blue",
+});
---
+++ text_animation/script.js
@@ -0,0 +1,12 @@
+gsap.from("h1",{
+    color: "red",
+    duration:2,
+    // delay:1,
+    x:35
+})
+gsap.from("p", {
+    opacity: 0,
+    duration: 2,
+    x:35,
+    stagger:1
+})
